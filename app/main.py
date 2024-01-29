f7fccc10662b14bc1bd1480cc4e8104a002fc0cf
Tushar Kuchhal <tushar.kuchhal@tabsquare.com>
2024-01-29 12:59:53 +0800

    doing a test end to end deploy

--- app/main.py
+++ app/main.py
@@ -32,4 +32,5 @@ def get_health(response: Response):
             'loading-config': os.getenv("CONFIGMAP_VARIABLE") if os.getenv("CONFIGMAP_VARIABLE") is not None else "fail",
             'loading-secrets': os.getenv("SECRETS_VARIABLE") if os.getenv("SECRETS_VARIABLE") is not None else "fail",
         },
+        'testing': True,
     }
