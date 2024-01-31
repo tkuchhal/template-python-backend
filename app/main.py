42c417de27f3ace2e9ae5b5c0acfc3572010cc61
Tushar Kuchhal <tushar.kuchhal@tabsquare.com>
2024-01-31 21:45:55 +0800

    adding human readable times

--- app/main.py
+++ app/main.py
@@ -26,11 +26,14 @@ def get_health(response: Response):
     if os.getenv("DB_CONNECTION_URL") is None or os.getenv("DB_USER") is None or os.getenv("DB_PASSWORD") is None:
         response.status_code = 500
 
+    os.environ["BUILD_TIME"] = pendulum.now().subtract(minutes=10).to_w3c_string()
+    os.environ["DEPLOY_TIME"] = pendulum.now().subtract(minutes=40).to_w3c_string()
+
     return {
         'current-time': pendulum.now().in_timezone('UTC').to_w3c_string(),
         'image-metadata': {
-            'build-time': os.getenv('BUILD_TIME'),
-            'deploy-time': os.getenv('DEPLOY_TIME'),
+            'build-time': pendulum.parse(os.getenv('BUILD_TIME')).diff_for_humans() + f" ({pendulum.parse(os.getenv('BUILD_TIME')).in_timezone('UTC')})",
+            'deploy-time': pendulum.parse(os.getenv('DEPLOY_TIME')).diff_for_humans() + f" ({pendulum.parse(os.getenv('DEPLOY_TIME')).in_timezone('UTC')})",
             'current-image': os.getenv('IMAGE_NAME'),
         },
         'health-check': {
