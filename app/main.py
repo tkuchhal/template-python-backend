3fd1627d2bd0206a453236ca75e4c66e21c51dcb
Tushar Kuchhal <tushar.kuchhal@tabsquare.com>
2024-01-31 10:58:40 +0800

    trying to save test artifact output

--- app/main.py
+++ app/main.py
@@ -23,6 +23,9 @@ def get_health(response: Response):
     if os.getenv("CONFIGMAP_VARIABLE") is None or os.getenv("SECRETS_VARIABLE") is None:
         response.status_code = 500
 
+    if os.getenv("DB_CONNECTION_URL") is None or os.getenv("DB_USER") is None or os.getenv("DB_PASSWORD") is None:
+        response.status_code = 500
+
     return {
         'current-time': pendulum.now().in_timezone('UTC').to_w3c_string(),
         'build-time': os.getenv('BUILD_TIME'),
@@ -31,6 +34,8 @@ def get_health(response: Response):
         'health-check': {
             'loading-config': os.getenv("CONFIGMAP_VARIABLE") if os.getenv("CONFIGMAP_VARIABLE") is not None else "fail",
             'loading-secrets': os.getenv("SECRETS_VARIABLE") if os.getenv("SECRETS_VARIABLE") is not None else "fail",
+        },
+        'db-connection-check': {
             'db-connection-url': os.getenv("DB_CONNECTION_URL") if os.getenv("DB_CONNECTION_URL") is not None else "fail",
             'db-user': os.getenv("DB_USER") if os.getenv("DB_USER") is not None else "fail",
             'db-password': os.getenv("DB_PASSWORD") if os.getenv("DB_PASSWORD") is not None else "fail",
---
+++ tests/dev/test_dev.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test_dev():
+    assert False
