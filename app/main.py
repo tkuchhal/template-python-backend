138072178fd181fbfa595419fe0c13c005ad24c4
Tushar Kuchhal <tushar.kuchhal@tabsquare.com>
2024-05-15 13:17:38 +0800

    added build time to health check endpoint

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def get_health():
                 'loading-config': configmap_variable,
                 'loading-secrets': secrets_variable,
             },
-            'status': 'healthy',
+            'status': 'ok',
         }
     except Exception as e:
         logger.error(f"An error occurred in get_health: {e}")
